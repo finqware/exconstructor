c37a7a43c975e4935e6ea36ecc0dcda6179c0c22
mitel <mitelone@gmail.com>
2019-11-25 13:35:34 +0200

    Reuse atoms where possible

---
+++ lib/utils.ex
@@ -0,0 +1,15 @@
+defmodule Utils do
+  @doc """
+    Attempts first to use an existing atom rathen than creating a new one.
+    Avoids a potential DDoS attack on the BEAM.
+
+    https://engineering.klarna.com/monitoring-erlang-atoms-c1d6a741328e
+  """
+  def to_existing_atom(string) do
+    try do
+      String.to_existing_atom(string)
+    rescue
+      _e in ArgumentError -> String.to_atom(string)
+    end
+  end
+end
